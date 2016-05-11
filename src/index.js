d38c84eddeb8b416be6894dc52a3f07315e0695c
Michele Prandina <michele@nevergiveapp.com>
2016-05-11 16:19:48 +0200

    Install youtube-search-api and create directory components with empty files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyAFH-k0VOXFbtqKPHIanxfgs4vmXe7SDGk';
 
 const App = () => {
-    return <div>Hi!</div>
+    return <div>Hello world, zio cane!</div>
 };
 
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+console.log(document.querySelector('.container'));
